4a5613370478c4d84eb64981e7f59d37f8973cd6
hurui <hurui@qq.com>
2022-04-20 17:06:19 +0800

    hot-fix commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("helo git2");
         System.out.println("helo git3");
+        System.out.println("hello git4");
     }
 }
